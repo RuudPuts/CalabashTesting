be40e930ebb0ba98a32a6ce3d1edafc60a339bc9
Ruud Puts <ruud.puts@philips.com>
2014-12-01 11:33:21 +0100

    Same for console.rb

--- console.rb
+++ console.rb
@@ -1,8 +1,28 @@
 #!/usr/bin/env ruby
-target = ARGV[0]
+target = ARGV.shift
+if target.nil?
+  puts "No target specified. Either run './run.rb ios' or './run.rb android'"
+  exit(false)
+else
+  target = target.downcase
+end
 
-Android_APK="prebuilt/CalabashDemo-debug.apk"
-iOS_APP="prebuilt/CalabashDemo-cal.app"
+if target == "android"
+  ANDROID_APK=%x(echo "$ANDROID_APK").strip
+  if ANDROID_APK.length == 0
+    puts "No Android APK specified. Please run 'export ANDROID_APK=\"YOUR_APK_FILE_PATH\"' first!"
+    exit(false)
+  end
+elsif target == "ios"
+  IOS_APP=%x(echo "$IOS_APP").strip
+  if IOS_APP.length == 0
+    puts "No iOS app specified. Please run 'export IOS_APP=\"YOUR_.APP_FILE_PATH\"' first!"
+    exit(false)
+  end
+end
+
+%x(unset ANDROID_APK)
+%x(unset IOS_APP)
 
 unless system("bundle version")
   puts "Can't find bundler. Check your ruby environment."
